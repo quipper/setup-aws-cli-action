ead557156bc86e11a5338f36128c58d70697813d
Hidetake Iwata <int128@gmail.com>
2021-05-21 16:18:04 +0900

    Add --update flag

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ cd "$(mktemp -d)"
 # https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
 curl -sf "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q awscliv2.zip
-sudo ./aws/install
+sudo ./aws/install --update
 aws --version
